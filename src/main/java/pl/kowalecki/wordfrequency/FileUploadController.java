5fefc522e162b858408a943213d95d578d0e893c
pkowalecki <kowalecki13pl@o2.pl>
2022-02-27 20:53:30 +0100

    - Max file size set to 500MB - ExceptionHandler with body message about too big file

--- src/main/java/pl/kowalecki/wordfrequency/FileUploadController.java
+++ src/main/java/pl/kowalecki/wordfrequency/FileUploadController.java
@@ -1,17 +1,22 @@
 package pl.kowalecki.wordfrequency;
 
+import com.sun.jdi.request.ExceptionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.LinkedHashMap;
 
 
 @Controller
+@ControllerAdvice
 public class FileUploadController {
 
     @Autowired
@@ -25,5 +30,10 @@ public class FileUploadController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleUploadError(){
+        return new ResponseEntity<>("File too big", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
