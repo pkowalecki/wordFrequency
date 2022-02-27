d63c5f1b0c7d90feac3e89d862e766457f0a901a
pkowalecki <kowalecki13pl@o2.pl>
2022-02-27 09:59:47 +0100

    Initial commit

---
+++ src/main/java/pl/kowalecki/wordfrequency/WordFrequencyApplication.java
@@ -0,0 +1,13 @@
+package pl.kowalecki.wordfrequency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordFrequencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordFrequencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kowalecki/wordfrequency/WordFrequencyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kowalecki.wordfrequency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordFrequencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
