8182cca056445331184ee4b94c5d6254db5a62c7
pkowalecki <kowalecki13pl@o2.pl>
2022-02-27 19:06:57 +0100

    .

--- src/main/java/pl/kowalecki/wordfrequency/FileUploadController.java
+++ src/main/java/pl/kowalecki/wordfrequency/FileUploadController.java
@@ -1,34 +1,29 @@
 package pl.kowalecki.wordfrequency;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-import javax.servlet.annotation.MultipartConfig;
-import java.util.TreeMap;
+import java.util.LinkedHashMap;
 
 
 @Controller
 public class FileUploadController {
 
-
     @Autowired
     private FileUploadService fileUploadService;
+
     @PostMapping("/uploadFile")
 
-    public ResponseEntity<TreeMap> uploadNewFile(@RequestParam("file") MultipartFile file){
-        try{
+    public ResponseEntity<LinkedHashMap> uploadNewFile(@RequestParam("file") MultipartFile file) {
+        try {
             fileUploadService.uploadFile(file);
             return new ResponseEntity<>(fileUploadService.countWordFreq(), HttpStatus.OK);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
--- src/main/java/pl/kowalecki/wordfrequency/FileUploadService.java
+++ src/main/java/pl/kowalecki/wordfrequency/FileUploadService.java
@@ -12,7 +12,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -23,31 +24,44 @@ public class FileUploadService {
     @Value("uploads")
     public String uploadDir;
 
-    public void uploadFile(MultipartFile file){
-        try{
+    public void uploadFile(MultipartFile file) {
+        try {
             Path fileLocation = Paths
                     .get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
             Files.copy(file.getInputStream(), fileLocation, StandardCopyOption.REPLACE_EXISTING);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new FileStorageException("Couldnt store file" + file.getOriginalFilename());
         }
 
     }
 
-    public TreeMap<String, Long> countWordFreq(){
+    public LinkedHashMap<String, Long> countWordFreq() {
         Pattern pattern = Pattern.compile("[a-zA-Z]+");
-        TreeMap<String, Long> treeMap= new TreeMap<>();
+        TreeMap<String, Long> treeMap = new TreeMap<>();
 
-        try(BufferedReader br = Files.newBufferedReader(Paths.get("uploads/wordFrequ.txt"))){
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("uploads/wordFrequ.txt"))) {
             br.lines()
                     .map(pattern::matcher)
                     .flatMap(Matcher::results)
                     .map(matchResult -> matchResult.group(0))
                     .collect(Collectors.groupingBy(String::toLowerCase, TreeMap::new, Collectors.counting()))
                     .forEach(treeMap::put);
-        }catch (IOException e){
+        } catch (IOException e) {
             System.err.format("IOException: %s%n", e);
         }
-        return treeMap;
+        return sortMap(treeMap);
     }
+
+    private LinkedHashMap<String, Long> sortMap(TreeMap<String, Long> treeMap) {
+
+        LinkedHashMap<String, Long> mapSorted = treeMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new));
+
+
+        return mapSorted;
+    }
+
 }
